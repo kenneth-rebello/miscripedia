8e5c9c69aa63f08a324f1d3af8a912d51ae95030
Kenneth Rebello <kentherebel07@gmail.com>
2025-09-08 23:23:19 +0530

    Close button for Expanded Miscrit

--- src/components/ExpandedMiscritCard.jsx
+++ src/components/ExpandedMiscritCard.jsx
@@ -75,10 +75,19 @@ const ExpandedMiscritCard = ({ miscrit, onClose }) => {
                                 </div>
                             ))}
                         </div>
-                        
+
                         {/* Mobile Image Section */}
                         <div className="md:hidden relative w-full h-[60vh] p-4 flex flex-col items-center justify-center">
-                            {selectedMiscritIndex !== null && (
+                            {selectedMiscritIndex === null ? (
+                                <button
+                                    onClick={onClose}
+                                    className="absolute top-2 left-2 z-10 p-3 text-white bg-black bg-opacity-50 rounded-full"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                                    </svg>
+                                </button>
+                            ) : (
                                 <button
                                     onClick={() => setSelectedMiscritIndex(null)}
                                     className="absolute top-2 left-2 z-10 p-2 text-white bg-black bg-opacity-50 rounded-full"
